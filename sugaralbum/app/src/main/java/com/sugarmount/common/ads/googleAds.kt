fd90e09df719f6a27964270e9962282b27690c97
jaewoo <mvgood21@gmail.com>
2022-10-13 22:19:53 +0900

    아이콘 생성 및 구글 리소스 등록

--- sugaralbum/app/src/main/java/com/sugarmount/common/ads/googleAds.kt
+++ sugaralbum/app/src/main/java/com/sugarmount/common/ads/googleAds.kt
@@ -1,7 +1,7 @@
 package com.sugarmount.common.ads
 
+import android.app.Activity
 import android.content.Context
-import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
@@ -12,11 +12,11 @@ import com.sugarmount.common.utils.log
 import com.sugarmount.sugaralbum.R
 
 
-class googleAds {
+class GoogleAds {
     companion object {
         private var mInterstitialAd: InterstitialAd? = null
 
-        fun loadAd(context: Context) {
+        fun loadInterstitialAd(context: Context) {
             // 전면
             val adRequest = AdRequest.Builder().build()
             InterstitialAd.load(
@@ -25,7 +25,7 @@ class googleAds {
                 adRequest,
                 object : InterstitialAdLoadCallback() {
                     override fun onAdFailedToLoad(adError: LoadAdError) {
-                        log.d(adError?.message)
+                        log.d(adError.message)
                         mInterstitialAd = null
                         val error = "domain: ${adError.domain}, code: ${adError.code}, " +
                                 "message: ${adError.message}"
@@ -37,6 +37,14 @@ class googleAds {
                     }
                 }
             )
+        }
+
+        fun showInterstitialAd(activity: Activity) {
+            if(mInterstitialAd != null) {
+                mInterstitialAd?.show(activity)
+            } else {
+                log.e("The interstitial ad wasn't ready yet.")
+            }
         }
 
     }
